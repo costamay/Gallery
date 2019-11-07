dda2f69d7269860d0eb97e21c1c13d9bd4c8a2d2
Titus Ouko <titusouko@gmail.com>
2019-11-07 04:29:04 +0300

    Add location class

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import datetime as dt
 
-# Create your models here.
+class Picture(models.Model):
+    title = models.CharField(max_length =30)
+    image = models.ImageField(upload_to = 'images/', default="")
+    posted_date= models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length =200)
+    location = models.ForeignKey('Location', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.description
+
+class Category(models.Model):
+    category = models.CharField(max_length =30)
+    
+    def __str__(self):
+        return self.category
+
+
+class Location(models.Model):
+    location = models.CharField(max_length =30)
+    
+    def __str__(self):
+        return self.category
