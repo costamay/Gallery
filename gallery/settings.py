a2adfe8af6f103b5e4fea0a59b9c732da345de24
Titus Ouko <titusouko@gmail.com>
2019-11-10 21:11:39 +0300

    Add migrations

--- gallery/settings.py
+++ gallery/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'photo.apps.PhotoConfig',
-    'bootstrap4',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
