3d3da0e904258d84945fab8d34ad19c8cbe56d5a
Titus Ouko <titusouko@gmail.com>
2019-11-10 23:48:02 +0300

    Add url path for about page

--- photo/urls.py
+++ photo/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns=[
     path('',views.index,name = 'index'),
+    path('about/', views.about, name='about'),
     path('search/', views.search_results, name='search_results'),
     path('image/<int:image_id>', views.view_image,name='view_image'),
     path('category/<int:id>', views.category,name='category')
